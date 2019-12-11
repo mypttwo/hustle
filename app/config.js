e3f2fa438487e84a578c29e5e2fc6e462b655133
mypttwo <mypt.two@gmail.com>
2019-12-11 15:58:54 +0530

    Adding basic infrastructure to use jwt

--- app/config.js
+++ app/config.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 
 const port = process.env.PORT;
 const dbConnStr = process.env.DBCONNSTR;
+const secret = process.env.SECRET;
 
 module.exports = {
     port,
-    dbConnStr
+    dbConnStr,
+    secret
 }
